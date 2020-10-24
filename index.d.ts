7bb09543d524789dbb73e85437e7c498fb69948f
EsunR <641411169@qq.com>
2020-10-24 14:28:56 +0800

    feat: 新增notMerge与lazyUpdate选项

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,8 @@ export const Echarts: React.FC<{
   height?: number | string;
   renderLoading?: () => React.ReactElement;
   onPress?: (e: any) => void;
+  notMerge?: boolean; // setOption 时是否启用 notMerge (默认 false)
+  lazyUpdate?: boolean; // setOption 时是否启用 lazyUpdate (默认 false)
 }>;
 
 export const echarts: any;
--- main/dist/index.js
+++ main/dist/index.js
@@ -24,7 +24,7 @@ class Echarts extends Component {
         isFirstLoad: false,
       };
     } else {
-      state.setOption(props.option);
+      state.setOption(props.option, props.notMerge, props.lazyUpdate);
       return null;
     }
   }
